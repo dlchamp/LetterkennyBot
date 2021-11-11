a7d43b5aa58649be2846a34a7db92b4c5f2141ba
dlchamp <36091350+dlchamp@users.noreply.github.com>
2021-11-10 18:11:31 -0600

    spell check

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ activity = nextcord.Activity(
 Start Discord event functions.
 '''
 
+
 @bot.event
 async def on_ready():
     await bot.change_presence(status=nextcord.Status.online, activity=activity)
