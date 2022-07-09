7279ee4e6b2527dafe01e544a1552a54cf9e77a8
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-07-09 18:53:14 -0500

    added Help class constructor..

--- letterkennybot/modules/help/help.py
+++ letterkennybot/modules/help/help.py
@@ -5,6 +5,9 @@ from disnake.ext.commands import Cog, slash_command
 class Help(Cog):
     """Class that represents help related command and events"""
 
+    def __init__(self, bot) -> None:
+        self.bot: InteractionBot = bot
+
     @Cog.listener()
     async def on_ready(self) -> None:
         """Invoked when this cog is loaded"""
@@ -16,21 +19,25 @@ class Help(Cog):
 
         embed = Embed(
             title=f"{self.bot.user.name} help",
-            description="Over 60 of your favorite chirps from seasons 1-10 of Letterkenny and season 1 of Shoresy",
+            description="Over 60 of your favorite chirps and quotes from seasons 1-10 of Letterkenny and season 1 of Shoresy",
         )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
         embed.add_field(
             name="Trigger Phrases:",
-            value='"Fuck you, Shoresy"\n"it\'s fucking embarrassing"\n"How are ya now"\n"To be fair"',
+            value='"Fuck you, Shoresy"\n"it\'s fucking embarrassing"\n"How are ya now"\n"To be fair"\nothers...',
+            inline=False,
         )
         embed.add_field(
             name="Commands:",
             value="`/remove` - Remove your stored member ID from the database",
+            inline=False,
         )
         embed.add_field(
             name="Private and Data Usage:",
             value="Using any of the trigger words will add your Discord member ID to the local database along with the ID of this guild. "
             'This data is only used for randomly selecting a member to mention in place of "Reilly" or "Jonesy" in certain quotes. '
             "This data is not used in any other way and you may remove it any time with the `/remove` command.",
+            inline=False,
         )
 
         await interaction.response.send_message(embed=embed)
