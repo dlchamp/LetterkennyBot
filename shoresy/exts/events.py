7327d52ee386015f39dd4ab148cefa652ac899f4
DLCHAMP <contact@dlchamp.com>
2025-04-18 12:55:17 -0500

    remove guild members from db on guild leave

--- shoresy/exts/events.py
+++ shoresy/exts/events.py
@@ -11,13 +11,21 @@ logger = log.get_logger(__name__)
 @plugin.listener()
 async def on_guild_join(guild: disnake.Guild) -> None:
     """Log when the bot has joined a guild."""
-    logger.info(f"Shoresy has joined {guild} ({guild.id})")
+    logger.info("Shoresy has joined %s (%s)", str(guild), str(guild.id))
 
 
 @plugin.listener()
 async def on_guild_remove(guild: disnake.Guild) -> None:
     """Log when the bot has left a guild."""
-    logger.info(f"Shoresy has left {guild} ({guild.id})")
+    await plugin.bot.remove_guild_members(guild.id)
+    logger.info("Shoresy has left %s (%s)", str(guild), str(guild.id))
+
+
+@plugin.listener()
+async def on_member_remove(member: disnake.Member) -> None:
+    '''Member left the guild.'''
+    await plugin.bot.remove_member(member, all_guilds=False)
+    logger.info('%s left %s and was removed from the database', str(member), str(member.guild))
 
 
 setup, teardown = plugin.create_extension_handlers()
